954b16d32d8c697bda4084f695a7d2c36338a28e
lenivoe <levishok@gmail.com>
2021-02-24 01:48:09 +0700

    fixed names of columns in the scripts

--- sql/tests.sql
+++ sql/tests.sql
@@ -5,7 +5,7 @@
 -- test 1
 delete from Documents_Document;
 select add_work_report(
-    _number => 'work report #1',
+    _number => 'work "report_id" #1',
     _id_project => 1
 );
 select add_contract(
@@ -36,19 +36,19 @@ select cancel_expired_tasks(1);
 
 -- test 5
 update Tasks_Task as t
-    set status = s.id
+    set "status_id" = s."id"
     from Tasks_Status as s
-    where s.name = 'активна'
-    returning t.id, t.project, t.description, t.status;
+    where s."name" = 'активна'
+    returning t."id", t."project_id", t.""description"", t."status_id";
 
-select t.id, t.project, t.description, t.deadline, s.name
-    from Tasks_Task as t join Tasks_Status as s on(t.status = s.id);
+select t."id", t."project_id", t.""description"", t."deadline", s."name"
+    from Tasks_Task as t join Tasks_Status as s on(t."status_id" = s."id");
 
 
 -- test 6
 select c as task_count, (e).*
     from get_most_available_employees_list(
-        (select id from Staff_Post where name = 'Дизайнер')
+        (select "id" from Staff_Post where "name" = 'Дизайнер')
     ) as eta(e, c);
 
 
@@ -56,9 +56,13 @@ select c as task_count, (e).*
 select *
     from get_employee_task_list(
         (
-            select id
+            select "id"
                 from Staff_Employee
-                where last_name = 'Васильченко' and first_name = 'Анна' and patronymic = 'Александровна'
+                where "last_name" = 'Васильченко' and "first_name" = 'Анна' and "patronymic" = 'Александровна'
         )
         , 'отменена'
     );
+
+
+-- ALTER TABLE tasks_task 
+-- RENAME COLUMN "prority_id" TO "priority_id";
