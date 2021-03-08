5a03eae367a88fb5ce3c84b5dc95e58785c2f8bf
lenivoe <levishok@gmail.com>
2021-03-08 18:32:15 +0700

    russification of client, documnt, project

--- ad_agency/ad_agency/agency/clients/admin.py
+++ ad_agency/ad_agency/agency/clients/admin.py
@@ -4,22 +4,35 @@ from advanced_filters.admin import AdminAdvancedFiltersMixin
 from .models import Client, Company, Order
 from staff.models import Employee
 
-from helper.utils import extract_employee_id
-from helper.utils import russify_columns
+from helper.utils import extract_employee_id, russify_columns
 
 admin.site.site_header = 'Ad-agency'
 admin.site.site_title = 'Ad-agency'
 
 
 class CompanyAdmin(AdminAdvancedFiltersMixin, admin.ModelAdmin):
-    list_display = ['name', 'address', 'OGRN', 'INN', 'requisites', 'phone']
     ordering = ['name']
     advanced_filter_fields = ('name')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # short descriptions
+        to_russian = [['name', 'Название'], ['address', 'Адрес'], ['OGRN', 'ОГРН'],
+                      ['INN', 'ИНН'], ['requisites', 'Реквизиты'], ['phone', 'Телефон']]
+        russify_columns(self, to_russian)
+
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ['last_name', 'first_name', 'patronymic', 'company', 'phone', 'email']
     ordering = ['last_name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # short descriptions
+        to_russian = [['last_name', 'Фамилия'], ['first_name', 'Имя'], ['patronymic', 'Отчество'],
+                      ['company', 'Компания'], ['phone', 'Телефон'], ['email', 'Электронная почта']]
+        russify_columns(self, to_russian)
 
 
 class OrderAdmin(admin.ModelAdmin):
--- ad_agency/ad_agency/agency/projects/admin.py
+++ ad_agency/ad_agency/agency/projects/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 
 from .models import Project
 
+from helper.utils import russify_columns
+
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['technical_task', 'client_order', 'creative_director', 'project_manager']
     ordering = ['client_order']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # short descriptions
+        to_russian = [['technical_task', 'Техническое задание'], ['client_order', 'Заказ'],
+                      ['creative_director', 'Креативный директор'], ['project_manager', 'Менеджер проекта']]
+        russify_columns(self, to_russian)
 
 
 admin.site.register(Project, ProjectAdmin)
